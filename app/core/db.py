4ce80018af16280bb3dd337fe4a821c22b500f5f
Quy Phan Quang <phanquangquy9a5@gmail.com>
2025-12-11 16:28:30 +0700

    fix: fix dockerfile

--- app/core/db.py
+++ app/core/db.py
@@ -3,16 +3,7 @@ import os
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
 
-POSTGRES_DB = os.getenv("POSTGRES_DB", "task_db")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "task_user")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "task_password")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5433")
-
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}",
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
     DATABASE_URL,
