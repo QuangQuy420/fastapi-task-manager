84cd9a4a87be5b5a9db768e47dc81a8fb0b80ab5
Quy Phan Quang <phanquangquy9a5@gmail.com>
2025-12-11 16:33:09 +0700

    fix: fix unit test

--- app/core/db.py
+++ app/core/db.py
@@ -3,7 +3,16 @@ import os
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "task_db")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "task_user")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "123456")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}",
+)
 
 engine = create_async_engine(
     DATABASE_URL,
